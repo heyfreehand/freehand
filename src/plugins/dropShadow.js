3b830f71d62b54e97520eed20adb85b355c546ba
Kyle Foster <hkylefoster@gmail.com>
2020-04-28 15:08:18 -0500

    Shadows

--- src/plugins/dropShadow.js
+++ src/plugins/dropShadow.js
@@ -7,11 +7,11 @@ export default function() {
     const utilities = _.fromPairs(
       _.map(theme('dropShadow'), (value, modifier) => {
         const className =
-          modifier === 'default' ? 'drop-shadow' : `${e(prefixNegativeModifiers('drop-shadow', modifier))}`
+          modifier === 'default' ? 'drop-shadow' : `drop-shadow\\:${modifier}`
         return [
           `.${className}`,
           {
-            'filter': `drop-shadow(${value})`,
+            'filter': Array.isArray(value) ? value.map(item => `drop-shadow(${item})`).join(' ') : `drop-shadow(${value})`
           },
         ]
       })
