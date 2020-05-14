b58ff9708001da4adab9d286261de03e8021659e
Kyle Foster <hkylefoster@gmail.com>
2020-05-14 13:50:56 -0500

    Buttons up transforms

--- src/plugins/transformTranslate.js
+++ src/plugins/transformTranslate.js
@@ -1,8 +1,9 @@
+// Done
 import createUtilityPlugin from '../util/createUtilityPlugin'
 
 export default function() {
   return createUtilityPlugin('transformTranslate', [
-    ['translate-x', ['--transform-translate-x']],
-    ['translate-y', ['--transform-translate-y']],
+    ['translate:x', ['--transform-translate-x']],
+    ['translate:y', ['--transform-translate-y']],
   ])
 }
